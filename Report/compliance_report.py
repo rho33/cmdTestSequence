8dd6442fc4d3c99040e69e19490407406fb7c26d
Ryan <rhohenhausen@gmail.com>
2020-10-06 23:34:59 -0700

    added check_report_data to main

--- Report/compliance_report.py
+++ Report/compliance_report.py
@@ -6,37 +6,13 @@ Arguments:
 
 Options:
   -h --help
-  -e
-  -v
-  -p
 """
 import sys
 sys.path.append('..')
-import warnings
-from report_data import get_report_data
+from report_data import get_report_data, check_report_data
 from report import add_compliance_section, ISection, build_report, add_test_specs
 import logfuncs as lf
 import filefuncs as ff
-
-# def get_compliance_report_data(paths, data_folder, docopt_args):
-#
-#     data = {}
-#     data['data_folder'] = data_folder
-#     data['report_type'] = rd.get_report_type(docopt_args, data_folder)
-#     data['merged_df'] = rd.get_merged_df(paths, data_folder)
-#     data['waketimes'] = rd.get_waketimes(paths)
-#     data['rsdf'] = rd.get_results_summary_df(data['merged_df'], data_folder, data['waketimes'])
-#     data['test_date'] = pd.to_datetime(data['test_specs_df'].loc['Test Start Date', 0]).date().strftime('%d-%b-%Y')
-#     data['area'] = float(data['test_specs_df'].loc['Screen Area (sq in)', 0])
-#     data['on_mode_df'] = get_on_mode_df(data['rsdf'], data['limit_funcs'], data['area'], data['report_type'])
-#     on_mode_df = rd.get_on_mode_df(data['rsdf'], data['limit_funcs'], data['area'], data['report_type'])
-#     # todo: force report type
-#     limit_funcs = rd.get_limit_funcs(report_type)
-#     hdr = 'hdr' in merged_df.test_name.unique()
-#
-#     area = float(data['test_specs_df'].loc['Screen Area (sq in)', 0])
-#     standby_df = get_standby_df(rsdf)
-
 
 def make_compliance_report(report_data):
     report = ISection(name='report')
@@ -44,14 +20,6 @@ def make_compliance_report(report_data):
     report = add_compliance_section(report, **report_data)
     report_name = f'compliance-report.pdf'
     build_report(report, report_name, **report_data)
-    
-
-def check_data(report_data, expected_data):
-    warnings.filterwarnings('always', category=UserWarning)
-    for item in expected_data:
-        if item not in report_data.keys():
-            msg = f'\nMissing Data Item: {item}\nReport may not be complete.'
-            warnings.warn(msg)
             
             
 def main():
@@ -71,7 +39,7 @@ def main():
         'waketimes',
         'test_specs_df',
     ]
-    check_data(report_data, expected_data)
+    check_report_data(report_data, expected_data)
     make_compliance_report(report_data)
     
 
