c303817c790f6a58fa89d742a64876fd1909fbf9
Ryan <rhohenhausen@gmail.com>
2020-10-21 13:24:48 -0700

    sample_models restructure

--- tests/run/run_report_exe.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-data_folder = sys.argv[1]
-exe_path = r'C:\Users\rhohe\PycharmProjects\cmdTestSequence\build\exe.win-amd64-3.6\report.exe'
-os.system(f'{exe_path} {data_folder}')
-os.chdir(data_folder)
-os.system('report.pdf')
