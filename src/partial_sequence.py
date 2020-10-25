164f0710d44abc0f8810921fe92ad6287edc2035
Ryan <rhohenhausen@gmail.com>
2020-10-24 22:28:45 -0700

    added save_sequences call, fixed log file name, fixed partial_test_seq_df index bug

--- src/partial_sequence.py
+++ src/partial_sequence.py
@@ -12,10 +12,11 @@ import pandas as pd
 import core.logfuncs as lf
 import core.filefuncs as ff
 import core.sequence.command_sequence as cs
+import core.sequence.sequence as ts
 
 
 def main():
-    logger, docopt_args, data_folder = lf.start_script(__doc__, 'compliance_report.log')
+    logger, docopt_args, data_folder = lf.start_script(__doc__, 'partial_sequence.log')
     paths = ff.get_paths(data_folder)
     test_seq_df = pd.read_csv(paths['test_seq'])
     tags = [int(i) for i in docopt_args['<tags>']]
@@ -24,12 +25,10 @@ def main():
     if paths['ccf'] is None:
         mask = mask | (test_seq_df['test_name'].apply(lambda name: 'ccf' in name))
     
-    partial_test_seq_df = test_seq_df[mask].reset_index()
-    partial_test_seq_df.to_csv('partial-ts.csv', index=False)
+    partial_test_seq_df = test_seq_df[mask].reset_index().drop('index', axis=1)
     command_df = cs.create_command_df(partial_test_seq_df)
+    ts.save_sequences(partial_test_seq_df, command_df, data_folder, partial=True)
     
-    command_df.to_csv('partial-cs.csv', index=False, header=False)
-
 
 if __name__ == '__main__':
     main()
