237eaf2d29e3ab3d9ded56d6c166faea4b61280d
Ryan <rhohenhausen@gmail.com>
2020-11-03 19:09:26 -0800

    added extra argument (9) to camera_ccf commands in command sequence

--- src/core/sequence/command_sequence.py
+++ src/core/sequence/command_sequence.py
@@ -186,9 +186,13 @@ def create_command_df(test_seq_df):
 
         if not pd.isnull(row['special_commands']):
             for special_command in row['special_commands'].split(','):
-
+                
+                
                 command_type, command = special_command.split(':')
-                command_rows.append((command_type, command.strip()))
+                if command_type == 'camera_ccf':
+                    command_rows.append((command_type, command.strip(), 9))
+                else:
+                    command_rows.append((command_type, command.strip()))
 
     command_df = pd.DataFrame(data=command_rows)
     command_df.columns = ['command_type', 'command', 'stab_wait', 'max_stab'][:command_df.shape[1]]
