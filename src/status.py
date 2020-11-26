a0e4781d00063b76f726953fd521111ea78f78ab
Ryan <rhohenhausen@gmail.com>
2020-11-26 09:56:55 -0800

    fixed bad get_merged_df call

--- src/status.py
+++ src/status.py
@@ -1,6 +1,6 @@
 """
 Usage:
-test_status.exe <data_folder> [options]
+status.exe <data_folder> [options]
 
 Arguments:
   data_folder       folder with test data, also destination folder
@@ -8,7 +8,6 @@ Arguments:
 Options:
   -h --help
 """
-from collections import defaultdict
 import pandas as pd
 import core.report.report_data as rd
 import core.logfuncs as lf
@@ -18,9 +17,10 @@ import core.filefuncs as ff
 def main():
     logger, docopt_args, data_folder = lf.start_script(__doc__, 'test_status.log')
     paths = ff.get_paths(data_folder)
-    
-    merged_df = rd.get_merged_df(paths, data_folder)
+
     test_seq_df = pd.read_csv(paths['test_seq'])
+    merged_df = rd.get_merged_df(test_seq_df, paths, data_folder)
+    
     rd.get_status_df.__wrapped__(test_seq_df, merged_df, paths, data_folder)
     
     
