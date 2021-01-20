7d3fa1f1acef6eb0c1543c2826ae986d14ae84fb
Ryan <rhohenhausen@gmail.com>
2021-01-20 13:07:51 -0800

    version number change

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if sys.platform == "win32":
 
 setup(
     name = 'TV Test System Scripts',
-    version = '0.14.1',
+    version = '0.14.2',
     description = "description",
     options = {
         "build_exe": build_exe_options,
